f618befe2e21e409e5cc64416b0715bf216d87dc
xyyx <xyyx@mail.ru>
2017-02-05 22:41:01 +0800

    Use FMRadio without wired headset/headphones

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -363,7 +363,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
      * @return true, antenna available; false, antenna not available
      */
     public boolean isAntennaAvailable() {
-        return mAudioManager.isWiredHeadsetOn();
+        return true;
     }
 
     private void setForceUse(boolean isSpeaker) {
